0d762bb50966a9d543d9478212c2a567b988b1e3
Nabeelaasarafali <144226627+Nabeelaasarafali@users.noreply.github.com>
2025-03-18 12:53:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f8ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.container {
+  text-align: center;
+}
+
+#city-input {
+  padding: 10px;
+  font-size: 16px;
+  width: 200px;
+  margin: 10px 0;
+}
+
+#weather-info {
+  margin-top: 20px;
+}
+
+#weather-icon {
+  font-size: 50px;
+}
